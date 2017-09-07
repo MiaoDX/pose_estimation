f6180160d7b375ebf765c980bb9772f8b9ccdda3
miaodx <miaodx@hotmail.com>
2017-09-07 13:54:57 +0800

    Use AlglibCluster as a library, enhance modularity of our code.

--- 5point_cmake/format_code.bat
+++ 5point_cmake/format_code.bat
@@ -1,4 +1,3 @@
 astyle  --style=stroustrup --suffix=none --recursive pose_estimation/*
 astyle  --style=stroustrup --suffix=none --recursive getRTAlgo/*
 astyle  --style=stroustrup --suffix=none *.cpp
-astyle  --style=stroustrup --suffix=none alglib/*.cpp alglib/*.h
